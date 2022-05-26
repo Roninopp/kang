d42f0bc44fe1676a569a127c40ba5dfbb738e47c
Roninopp <99870677+Roninopp@users.noreply.github.com>
2022-05-26 11:06:03 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -36,9 +36,9 @@ Add me to your group and play music freely!**
      disable_web_page_preview=True
     )
 
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("roninstart") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**Group Music Player Online ✅**""",
+      await message.reply_text("""**fantastic Music Player Online ✅**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
